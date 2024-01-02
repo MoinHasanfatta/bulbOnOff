b31a2ed999872a8f9a362c2a03bd9e65901a1ee0
MoinHasanfatta <hasanfattamoin@gmail.com>
2024-01-02 22:26:58 +0530

    first commit

---
+++ bulb.js
@@ -0,0 +1,11 @@
+//function for on
+
+function bulbOn(){
+    let On = document.getElementById('bulbOff')
+    On.setAttribute("src", "on.jpg")
+}
+
+function bulbOff(){
+    let Off = document.getElementById('bulbOff')
+    Off.setAttribute("src", "off.jpg")
+}
